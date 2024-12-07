5255c1d19eb6a195729af0e90d9bf1808ad1c358
Harsh Sharma <harsh@codelif.in>
2024-12-08 00:27:39 +0530

    fix:  header not full width on big screens

--- jportal/src/components/Header.jsx
+++ jportal/src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = ({ setIsAuthenticated }) => {
 
   return (
     <header className="bg-[#191c20] mx-auto px-3 pt-4 pb-2">
-      <div className="container flex justify-between items-center">
+      <div className="container-fluid flex justify-between items-center">
         <h1 className="text-white text-2xl font-bold lg:text-3xl font-sans">
           JPortal
         </h1>
