226367f8ae3a90514182996d7b8dc19ed8d311a9
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-07-08 13:21:49 +0530

    fixed down swipe issue

--- jportal/src/components/AttendanceCard.jsx
+++ jportal/src/components/AttendanceCard.jsx
@@ -292,7 +292,7 @@ const AttendanceCard = ({
       >
         <SheetContent
           side="bottom"
-          className="h-[70vh] bg-[var(--card-bg)] text-[var(--text-color)] border-0 overflow-hidden rounded-t-3xl shadow-xl p-0"
+          className="min-h-[70vh] bg-[var(--card-bg)] text-[var(--text-color)] border-0 overflow-hidden rounded-t-3xl shadow-xl p-0"
         >
           <SheetHeader>
             {/* <SheetTitle className="text-white">{}</SheetTitle> */}
