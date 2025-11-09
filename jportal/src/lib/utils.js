227a6d2347cf852f7638cc957fa26366ec420a7e
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-11-09 19:58:01 +0530

    add utility functions for decimal formatting and GPA calculations -- abstraction for the general settings

--- jportal/src/lib/utils.js
+++ jportal/src/lib/utils.js
@@ -4,3 +4,26 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export function getDecimalPlaces() {
+  return 2;
+}
+
+export function formatDecimal(value, places = 2) {
+  if (typeof value === "number") {
+    return value.toFixed(places);
+  }
+  return value;
+}
+
+export function getAttendanceDecimal() {
+  return 2;
+}
+
+export function getGpaDecimal() {
+  return 2;
+}
+
+export function getTargetGpaDecimal() {
+  return 2;
+}
